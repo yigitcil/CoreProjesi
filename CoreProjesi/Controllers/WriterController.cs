174c9075f6f9defd14a7ca8cd340b249ebc22397
yigitcil <yigit.cilesiz@tedu.edu.tr>
2024-03-07 11:58:22 +0300

    loginChnaged

---
+++ CoreProjesi/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProjesi.Controllers
+{
+	[Authorize]
+	public class WriterController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult WriterProfile()
+		{
+			return View();
+		}
+		public IActionResult WriterMail() {
+			return View();
+		}
+	}
+}
--- CoreProjesi/Program.cs
+++ CoreProjesi/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +18,36 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+ void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllersWithViews();
+
+    services.AddSession();
+
+    services.AddMvc(config=>
+    {
+        var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+        config.Filters.Add(new AuthorizeFilter(policy));
+
+    });
+    services.AddMvc();
+    services.AddAuthentication(
+        CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(x=>
+        {
+            x.LoginPath = "/Login/Index/";
+        }
+        );
+}
+void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+{
+	app.UseSession();
+	app.UseMvcWithDefaultRoute();
+}
+//app.UseSession();
+
 app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
 
 app.UseHttpsRedirection();
@@ -24,6 +59,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Blog}/{action=Index}/{id?}");
 
 app.Run();
